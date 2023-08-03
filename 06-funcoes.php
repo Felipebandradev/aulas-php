6b89d4a43bbb5346cc2387293d82e7dfe23888c6
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-08-03 09:28:21 -0300

    Funções com parâmetro ( ou argumento)

--- 06-funcoes.php
+++ 06-funcoes.php
@@ -41,6 +41,36 @@
         ?>
         <p>Estamos no curso <?=dados_curso()?></p>
         <p><?=dados_curso()?> é ministrado no Senac Penha.</p>
+
+        <h2>Funções com parâmetro ( ou argumento)</h2>
+        <?php
+        function soma( $valor1, $valor2, $valor3){
+           // variavel de escopo local 
+           return $total = $valor1 + $valor2 + $valor3;
+        }
+        ?>
+        <!-- Chamando e exibindo o retorno  -->
+        <p>Resultado 1: <?=soma(10, 20, 5)?></p>
+        <p>Resultado 2: <?=soma(23, 84, 25)?></p>
+
+
+        <?php 
+        // chamando e guardando o resultado
+        $resultado3 = soma(500,480,700);
+        ?>
+
+        <p>Resultado 3: <?=$resultado3?></p>
+        
+        <?php if($resultado3 > 100) {  ?>
+            <p>Vinícius foi demitido!</p>
+        <?php }else{ ?>
+            <p>Vinícius recebe aumento!</p>
+        <?php } ?>
+        
+        <?php if ( soma(2, 3, 10) >= 10 ){?>
+            <p>Kauê foi Reprovado!!!</p>
+        <?php } ?>
+
     </main>
 </body>
 </html>
