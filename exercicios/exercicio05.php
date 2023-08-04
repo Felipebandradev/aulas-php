423625d860acdae52a9815b7b32af9fd31e66a37
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-08-04 08:51:18 -0300

    formatação geral do ex05

--- exercicios/exercicio05.php
+++ exercicios/exercicio05.php
@@ -1,37 +1,44 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Média de alunos</title>
 
     <style>
-        .aprovado{
-            color:blue;
+        .aprovado {
+            color: blue;
         }
-        .reprovado{
+
+        .reprovado {
             color: red;
         }
 
-        table{
+        table {
             width: 50%;
             margin: auto;
         }
 
-        tr{   
+        tr {
             text-align: center;
             font-size: 2rem;
         }
 
-        table,td,th{ border: black 1px solid;}
+        table,
+        td,
+        th {
+            border: black 1px solid;
+        }
 
-        tr:first-child{
-                    color:honeydew;
-                    background-color:darkorchid;
-                }
+        tr:first-child {
+            color: honeydew;
+            background-color: darkorchid;
+        }
     </style>
 
 </head>
+
 <body>
     <table>
         <tr>
@@ -42,52 +49,57 @@
             <th>situação</th>
         </tr>
         <?php
-        $alunos = [ 
-            [   "nome"  =>  "Moura",
+        $alunos = [
+            [
+                "nome"  =>  "Moura",
                 "nota1" => 8,
                 "nota2" => 6
-            ], 
-            ["nome" => "Aline",
-            "nota1" => 10,
-            "nota2" => 10
+            ],
+            [
+                "nome" => "Aline",
+                "nota1" => 10,
+                "nota2" => 10
             ],
             [
                 "nome" => "Barbosa",
                 "nota1" => 10,
-                "nota2" => 10 
+                "nota2" => 10
             ],
-            ["nome" => "Eliel",
-            "nota1" =>  0,
-            "nota2" => 8
+            [
+                "nome" => "Eliel",
+                "nota1" =>  0,
+                "nota2" => 8
             ],
-            [   
-                "nome" =>"Valeska",
+            [
+                "nome" => "Valeska",
                 "nota1" => 3,
                 "nota2" => 7
-            ] 
+            ]
         ];
 
-        $resultado = function (int $nota1, $nota2 ){
-        return  ($nota1 + $nota2)/2;
+        $resultado = function (int $nota1, $nota2) {
+            return ($nota1 + $nota2) / 2;
         };
-    
-        function situacao( $resultados ){ 
-        if ( $resultados >= 7 ){ 
-        return "aprovado";        
-        } else{ 
-            return "reprovado";
-        } 
-        } 
 
-        foreach ($alunos as $aluno){ ?>
+        function situacao($resultados)
+        {
+            if ($resultados >= 7) {
+                return "aprovado";
+            } else {
+                return "reprovado";
+            }
+        }
+
+        foreach ($alunos as $aluno) { ?>
             <tr>
-                <td><?=$aluno["nome"]?></td>
-                <td><?=$aluno["nota1"]?></td>
-                <td><?=$aluno["nota2"]?></td>
-                <td><?=$resultado($aluno["nota1"],$aluno["nota2"])?></td>
-                <td><span class="<?=situacao($resultado($aluno["nota1"],$aluno["nota2"]))?>"><?=situacao($resultado($aluno["nota1"],$aluno["nota2"]))?></span></td>
+                <td><?= $aluno["nome"] ?></td>
+                <td><?= $aluno["nota1"] ?></td>
+                <td><?= $aluno["nota2"] ?></td>
+                <td><?= $resultado($aluno["nota1"], $aluno["nota2"]) ?></td>
+                <td><span class="<?= situacao($resultado($aluno["nota1"], $aluno["nota2"])) ?>"><?= situacao($resultado($aluno["nota1"], $aluno["nota2"])) ?></span></td>
             </tr>
         <?php } ?>
     </table>
 </body>
+
 </html>
