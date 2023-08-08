8efc9495889c285bf0d4c91270b5a94bc4361af4
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-08-08 12:02:26 -0300

    exemplo Formularios Post

--- processa-post.php
+++ processa-post.php
@@ -7,25 +7,46 @@
 </head>
 <body>
     <main>
-        <pre><?=var_dump($_POST) ?></pre>
         <h1>Exemplo de usando post</h1>
         <hr>
         <p>Receber e processar dados via <b>Post</b></p>
 
-        <?php 
+        <?php
+        // verificar se os campos nome e eamil estão vazios 
+        if( empty($_POST["nome"]) || empty($_POST["email"]) ){ ?>
+            <p  style=" color: red">Você deve preencher nome e e-mail!!!</p>
+            <p><a href="10-fomulario-exemplo-post.html">Voltar</a></p>
+        <?php    
+        } else {
+
         // Capturandos dados transmitidos
         $nome = $_POST["nome"];
         $email = $_POST["email"];
+        $idade = $_POST["idade"];
         $mensagem = $_POST["menssagem"];
-        ?>
+        $interesses = $_POST["interesses"];
 
+        
+        ?>
+        
     <h2>Dados:</h2>
 
     <ul>
         <li>Nome: <?=$nome?></li>
         <li>E-mail: <?=$email?></li>
+        <li> Idade : <?=$idade?></li>
+
+        <?php if( !empty($_POST["interesses"])){ ?>
+        <li> interesses : <?=implode(", ", $interesses)?></li>
+        <?php } ?>
+
+        <?php
+        /* Se a menssagem não estiver vazia mostre a menssagem */ 
+         if( !empty($mensagem)){?>
         <li>Mensagem: <?=$mensagem?></li>
+        <?php }; ?>
     </ul>
+    <?php }?>
     </main>
 
 
